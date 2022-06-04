d3da58501507bda8d41545fa7f45c0687758e945
aventailll <93695475+aventailll@users.noreply.github.com>
2022-06-05 01:09:15 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN = os.getenv('OTA1NjU3Mzc0NzM4NjEyMjk2.YYNRKA.VNclXAHW2AMdqglQ8u5tuSgi4Ls')
 client = discord.Client() #gets connect to Discord
 
 @client.event #is used to register an event
@@ -17,11 +17,11 @@ async def on_ready(): #is called when the bot is ready to start being used
 async def on_message(message): #event triggers each time a message is received
     if message.author == client.user:
         return
-    if message.content.startswith('$league'): #sees if the beginning of the message starts with '$league' to trigger the command
+    if message.content.startswith(':)league'): #sees if the beginning of the message starts with '$league' to trigger the command
         username = message.content[8:] #concatenates the beginning of the message to only get the username
         league_stats = get_rank(username) #uses the get_rank function to get the data about the user
         await message.channel.send(league_stats) #prints the users data into discord
-    if message.content.startswith('$val'):
+    if message.content.startswith(':)val'):
         username = message.content[5:]
         valorant_stats = val_stats(username)
         await message.channel.send(valorant_stats)
